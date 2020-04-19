670592c928e9846f00622eaf69613c86572dfde6
blake <ade.li.indie@gmail.com>
2020-04-19 17:35:19 +0800

    通过文件读取配置

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/DGHeroin/gweb
 
 go 1.12
 
-require github.com/kardianos/service v1.0.0
+require (
+	github.com/kardianos/service v1.0.0
+	github.com/sirupsen/logrus v1.5.0
+)
--- gweb.go
+++ gweb.go
@@ -1,10 +1,16 @@
 package main
+
 import (
+    "encoding/json"
     "flag"
     "fmt"
     "github.com/kardianos/service"
+    logger "github.com/sirupsen/logrus"
+    "io/ioutil"
     "net/http"
     "os"
+    "path/filepath"
+    "runtime"
 )
 
 var (
@@ -12,10 +18,12 @@ var (
     port = flag.Int("p", 8080, "port")
 )
 
-var logger = service.ConsoleLogger
-
-type program struct {
+type AppConfig struct {
+    Dir string
+    Port int
 }
+
+type program struct {}
 
 func (p *program) Start(s service.Service) error {
     go p.run()
@@ -23,7 +31,7 @@ func (p *program) Start(s service.Service) error {
 }
 
 func (p *program) run() {
-    loginFn()
+    startServe()
 }
 
 func (p *program) Stop(s service.Service) error {
@@ -32,8 +40,8 @@ func (p *program) Stop(s service.Service) error {
 
 func main() {
     svcConfig := &service.Config{
-        Name:        "gweb",                 //服务显示名称
-        DisplayName: "gweb Service",         //服务名称
+        Name:        "gweb",                              //服务显示名称
+        DisplayName: "gweb Service",                      //服务名称
         Description: "Anther other static file service.", //服务描述
     }
 
@@ -46,9 +54,9 @@ func main() {
     if err != nil {
         logger.Error(err)
     }
-
     if len(os.Args) > 1 {
-        switch os.Args[1] {
+        action := os.Args[1]
+        switch action {
         case "install":
             s.Install()
             logger.Info("服务安装成功!")
@@ -82,24 +90,74 @@ func main() {
             } else {
                 logger.Infof("%s %v", s, st)
             }
-        case "dummy":
+        case "config":
+            setConfig()
             return
         }
-
         return
     }
+
     err = s.Run()
     if err != nil {
         logger.Error(err)
     }
 }
-
-func loginFn() {
+func setConfig() {
+    os.Args = os.Args[1:]
     flag.Parse()
-    http.Handle("/", http.FileServer(http.Dir(*path)))
-
-    err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+    cfg := AppConfig{}
+    cfg.Dir = *path
+    cfg.Port = *port
+    data, _ := json.Marshal(&cfg)
+    cfgPath := fmt.Sprintf("%v/.gweb.json", getPath())
+    if err := ioutil.WriteFile(cfgPath, data, 0600); err != nil {
+        logger.Error(err)
+    }
+    logger.Println("写配置成功:", getPath(), string(data))
+}
+func getConfig() (*AppConfig, error) {
+    cfg := &AppConfig{
+        Dir:  "./",
+        Port: 8080,
+    }
+    cfgPath := fmt.Sprintf("%v/.gweb.json", getPath())
+    data, err := ioutil.ReadFile(cfgPath)
     if err != nil {
-        fmt.Println(err)
+        cfg := AppConfig{
+            Dir:  "./",
+            Port: 8080,
+        }
+        data, err := json.Marshal(&cfg)
+        ioutil.WriteFile(cfgPath, data, 0600)
+        return nil, err
+    }
+    err = json.Unmarshal(data, &cfg)
+    return cfg, err
+}
+
+func getPath() string {
+    switch runtime.GOOS {
+    case "linux":
+        os.Mkdir("/etc/gweb/", 0600)
+        return "/etc/gweb/"
+    default:
+        dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+        if err != nil {
+            panic(err)
+        }
+        return dir
+    }
+}
+
+func startServe() {
+    cfg, err := getConfig()
+    if err != nil {
+        logger.Error(err)
+        return
+    }
+    http.Handle("/", http.FileServer(http.Dir(cfg.Dir)))
+    err = http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil)
+    if err != nil {
+        logger.Error(err)
     }
 }
